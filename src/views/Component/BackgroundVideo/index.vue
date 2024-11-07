2177531570d677f57277c0582e51ab7dd5b2f9c3
guoziying <guoziying@caacitc.com>
2024-11-07 10:23:08 +0800

    视频背景

---
+++ src/views/Component/BackgroundVideo/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div ref="BackgroundVideoBoxRef" class="BackgroundVideoBox">
+    <video
+      autoplay
+      muted
+      loop
+      preload="auto"
+      src="@/assets/views/Component/BackgroundVideo/BackgroundVideo-3.mp4"
+    />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="scss" scoped>
+.BackgroundVideoBox {
+  width: 100vw;
+  height: 100vh;
+  box-sizing: border-box;
+  overflow: hidden;
+  video {
+    width: 100vw;
+    height: 100vh;
+    object-fit: fill;
+  }
+}
+</style>
